bad1d744a6594d98234714aeda7414a55546a69f
Dinesh-Talwadker <talwadkerdinesh@gmail.com>
2022-07-06 14:10:11 +0530

    Added basic moderation commands

--- main.py
+++ main.py
@@ -6,15 +6,13 @@ from discord.ext import tasks, commands
 from discord.utils import get
 from itertools import cycle
 
-prefix = './'
-#Comman prefix is setup here, this is what you have to type to issue a command to the bot
+prefix = '!'
+
 bot = commands.Bot(command_prefix = prefix)
 
-#Removed the help command to create a custom help guide
 bot.remove_command('help')
 
-#Variable containing statuses for the bot to cycle through
-status = cycle(["status 1", "status 2", "status3"])
+status = cycle(["listening to yo mom", "listening to !help", "UwU"])
 
 #|--------------------EVENTS--------------------|
 
@@ -26,9 +24,8 @@ async def on_ready():
     print("Bot is ready to use.")
 
 @bot.event
-async def on_member_join(member):
-    #When a member joins the discord, they will get mentioned with this welcome message
-    print(f'Member {member.mention} has joined!')
+async def on_member_join(member):    
+    print(f'Welcome {member.mention}!, glad to have you here!')
 
 @bot.event
 async def on_message(message):
@@ -36,20 +33,14 @@ async def on_message(message):
         print("This is a command")
         await bot.process_commands(message)
     else:
-        with open("words_blacklist.txt") as bf:
-            blacklist = [word.strip().lower() for word in bf.readlines()]
-        bf.close()
+        # with open("words_blacklist.txt") as bf:
+        #     blacklist = [word.strip().lower() for word in bf.readlines()]
+        # bf.close()
+        # blacklist words here 
 
-        channel = message.channel
-        for word in blacklist:
-            if word in message.content:
-                bot_message = await channel.send("message contains profanity, deleting ...")
-                await message.delete()
-                await asyncio.sleep(3)
-                await bot_message.delete()
+        channel = message.channel        
                 
     await bot.process_commands(message)
-
 
 #This event waits for commands to be issued, if a specific command requires a permission or arguement
 #This event will be invoked to tell the user that they dont have the required permissions
@@ -75,30 +66,31 @@ async def on_command_error(context, error):
 async def help(message):
     helpC = discord.Embed(title="moderator Bot \nHelp Guide", description="discord bot built for moderation")
     helpC.set_thumbnail(url='https://i.imgur.com/ZOKp8LH.png')
-    helpC.add_field(name="Clear", value="To use this command type ./clear and the number of messages you would like to delete, the default is 5.", inline=False)
-    helpC.add_field(name="kick/ban/unban", value="To use this command type ./kick/ban/unban then mention the user you would like to perform this on, NOTE: user must have permissions to use this command.", inline=False)
+    helpC.add_field(name="clear", value="To use this command type !clear and the number of messages you would like to delete, the default is 5.", inline=False)
+    helpC.add_field(name="kick/ban/unban", value="To use this command type !kick, !ban, !unban then mention the user you would like to perform this on, NOTE: user must have permissions to use this command.", inline=False)
 
     await message.channel.send(embed=helpC)
 
 @bot.command()
-#Checks whether the user has the correct permissions when this command is issued
 @commands.has_permissions(manage_messages=True)
 async def clear(context, amount=5):
     await context.channel.purge(limit=amount+1)
 
-#Kick and ban work in a similar way as they both require a member to kick/ban and a reason for this
-#As long as the moderator has the right permissions the member will be banned/kicked
 @bot.command()
 @commands.has_permissions(kick_members=True)   
 async def kick(context, member : discord.Member, *, reason=None):
     await member.kick(reason=reason)
-    await context.send(f'Member {member} kicked')
+    await context.send(f'Member {member} has been kicked')
 
 @bot.command()
 @commands.has_permissions(ban_members=True)   
 async def ban(context, member : discord.Member, *, reason=None):
     await member.ban(reason=reason)
     await context.send(f'{member} has been banned')
+
+@bot.command()
+async def calc(context, *, equation):        
+    await context.send(f'Answer: ' + eval(equation))
 
 #Unbanning a member is done via typing ./unban and the ID of the banned member
 @bot.command()
@@ -133,12 +125,10 @@ async def blacklist_add(context, *, word):
     await context.send(f'Word "{word}" added to blacklist.')
 
 #|------------------TASKS------------------| 
-#This is a task which runs every 5 seconds (change this to however long you require
+#This is a task which runs every 5 seconds 
 @tasks.loop(seconds=5)
 async def change_status():
     #This loops through the status variable at the top of this file every 5 seconds
     await bot.change_presence(activity=discord.Game(next(status)))
 
-#Enter your bot token from discord here, so when the code runs, your discord bot will come online
-bot.run('')
-
+bot.run('BOT_TOKEN_HERE')
